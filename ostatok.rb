8c349201f4323ebe385199fcbae09bbcab514121
Ramil Karimov <karimovr@inbox.ru>
2015-09-26 16:27:58 +0300

    0

--- ostatok.rb
+++ ostatok.rb
@@ -1,9 +1,8 @@
 puts "Введите число"
 number = gets.chomp.to_i
 result = number % 2
-if result == 0
+  if result == 0
 puts "Число четное"
-elsif result == 1
-	
+  elsif result == 1	
 puts "Число нечетное" 
-end
+  end
